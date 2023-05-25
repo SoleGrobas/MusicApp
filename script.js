e88117aee087b9323d7306cd9465f24e309b916f
Anais-RV <anaisrv@gmail.com>
2023-05-25 15:16:52 +0200

    25.05.23

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-//function playSound() {
-  //  var audio = document.getElementById("myAudio");
-   // audio.play();
- // }
-
- function playSound(soundSrc) {
-  var sound = document.getElementById("myAudio");
-  
-  // Cargar el sonido
-  sound.src = soundSrc;
-  sound.load();
-
-  // Reproducir el sonido
-  sound.play();
+function playtom1() {
+  var audio = document.getElementById("tom1");
+  audio.play();
 }
+
+function playtom2() {
+  var audio = document.getElementById("tom2");
+  audio.play();
+}
+
+function playtom3() {
+  var audio = document.getElementById("tom3");
+  audio.play();
+}
+
+function playtom4() {
+  var audio = document.getElementById("tom4");
+  audio.play();
+}
+
+function playcrash() {
+  var audio = document.getElementById("crash");
+  audio.play();
+}
+
+function playhithat() {
+  var audio = document.getElementById("hithat");
+  audio.play();
+}
+
+function playbombo() {
+  var audio = document.getElementById("bombo");
+  audio.play();
+}
+
+function playredoblante() {
+  var audio = document.getElementById("redoblante");
+  audio.play();
+}
+
+
