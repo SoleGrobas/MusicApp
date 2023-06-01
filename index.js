142a90515843182d3bdf0f67bea4ec5cdb7b5753
MaiteNavas <maitenavas626@gmail.com>
2023-06-01 14:15:56 +0200

    responsive drum

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-function playtom1() {
-  var audio = document.getElementById("tom1");
-  audio.play();
-}
-
-function playtom2() {
-  var audio = document.getElementById("tom2");
-  audio.play();
-}
-
-function playtom3() {
-  var audio = document.getElementById("tom3");
-  audio.play();
-}
-
-function playtom4() {
-  var audio = document.getElementById("tom4");
-  audio.play();
-}
-
-function playcrash() {
-  var audio = document.getElementById("crash");
-  audio.play();
-}
-
-function playhithat() {
-  var audio = document.getElementById("hithat");
-  audio.play();
-}
-
-function playbombo() {
-  var audio = document.getElementById("bombo");
-  audio.play();
-}
-
-function playredoblante() {
-  var audio = document.getElementById("redoblante");
+function play(soundId) {
+  var audio = document.getElementById(soundId);
   audio.play();
 }
 
